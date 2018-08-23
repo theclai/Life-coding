2e09e7bf7bca9c76b9c11ef387e7af2da0ca8e9d
nanofaroque <faroque.ae@gmail.com>
2018-08-23 15:28:59 -0500

    gbus problem and start

---
+++ src/kickstart/GbusSolution.java
@@ -0,0 +1,25 @@
+package kickstart;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class GbusSolution {
+    public static void main(String[] args){
+        readInput("/Users/omarfaroque/Life-coding/src/kickstart/input_gbus.txt");
+    }
+
+    private static void readInput(String s) {
+        List<String> lines;
+        try {
+            lines = Files.readAllLines(Paths.get(s), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("someting");
+
+    }
+}
