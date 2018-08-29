cb79a06fe79a98381faf004715903f0d0d3fdb7f
nanofaroque <faroque.ae@gmail.com>
2018-08-29 17:46:58 -0500

    adding min and remove method

--- src/data_structure/heap/MinHeap.java
+++ src/data_structure/heap/MinHeap.java
@@ -4,17 +4,17 @@ import java.util.Arrays;
 
 public class MinHeap {
     int[] data;
-    int size=0;
-    int capacity=10;
+    int size = 0;
+    int capacity = 10;
 
     public MinHeap() {
         data = new int[capacity];
     }
 
-    public void ensureExtraCapacity(){
-        if(size==capacity){
-            data=Arrays.copyOf(data,capacity*2);
-            capacity*=2;
+    public void ensureExtraCapacity() {
+        if (size == capacity) {
+            data = Arrays.copyOf(data, capacity * 2);
+            capacity *= 2;
         }
     }
 
@@ -42,21 +42,52 @@ public class MinHeap {
         return 2 * parent + 2;
     }
 
-    public boolean hasLeftChild(int index){
-        return getLeftChildIndex(index)<size;
+    public boolean hasLeftChild(int index) {
+        return getLeftChildIndex(index) < size;
     }
 
-    public boolean hasRightChild(int index){
-        return getRightChildIndex(index)<size;
+    public boolean hasRightChild(int index) {
+        return getRightChildIndex(index) < size;
     }
 
-    public boolean hasParent(int index){
-        return getParent(index)>=0;
+    public boolean hasParent(int index) {
+        return getParent(index) >= 0;
     }
+
     public void swap(int indexOne, int indexTwo) {
         int temp = data[indexOne];
         data[indexOne] = data[indexTwo];
         data[indexTwo] = temp;
+    }
+
+
+    public int min() {
+        if (size == 0) throw new IllegalArgumentException("There is no data in the array");
+        return data[0];
+    }
+
+    /**
+     * find the min element and remove from the data
+     */
+    public int minAndRemove() {
+        // find the min
+        int min = min();
+
+        // get the last element
+        int last = data[size - 1];
+        // move it to the first positon
+        data[0] = last;
+        // shrink the array since one item has been removed
+        size--;
+        heapifyDown();
+        return min;
+
+    }
+
+    /**
+     * It heapify from top to bottom since violation happened on the top
+     */
+    private void heapifyDown() {
     }
 
     /**
