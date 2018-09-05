5d9e8c6cd59dd4dd41e875fd631f2b374c6b9c2d
nanofaroque <faroque.ae@gmail.com>
2018-09-05 14:43:00 -0500

    answering stackoverflow questions

---
+++ src/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private int age;
+    Cat(){
+        age =0;
+    }
+
+    Cat(int a){
+        age=a;
+    }
+
+}
---
+++ src/RandomTest.java
@@ -0,0 +1,6 @@
+public class RandomTest {
+    public static void main(String[] args){
+        Cat cat1 = new Cat(9);
+        System.out.println(cat1.toString());
+    }
+}
