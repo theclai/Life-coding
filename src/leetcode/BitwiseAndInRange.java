e787b5d2b2478355d5b0e68467565f32fa5ba340
Omar Faroque <faroque.ae@gmail.com>
2018-09-07 18:17:09 -0500

    finishing bitwise and in range

--- src/leetcode/BitwiseAndInRange.java
+++ src/leetcode/BitwiseAndInRange.java
@@ -5,10 +5,22 @@ public class BitwiseAndInRange {
         int low=5;
         int high=7;
         computeNaive(low,high);
-        advanceCompute(low,high);
+        System.out.println(advanceCompute(low,high));
     }
 
-    private static void advanceCompute(int low, int high) {
+
+
+    private static int advanceCompute(int low, int high) {
+        int count=0;
+        while(low!=high){
+            low >>= 1;
+            high >>= 1;
+            count++;
+        }
+        System.out.println(low << count);
+        return low<<count;
+
+
     }
 
     private static void computeNaive(int low, int high) {
