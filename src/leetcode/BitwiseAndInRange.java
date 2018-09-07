7e69fe143bc4d084e0182ddd0e9eb3085714fb64
Omar Faroque <faroque.ae@gmail.com>
2018-09-07 17:26:01 -0500

    advance impl start

--- src/leetcode/BitwiseAndInRange.java
+++ src/leetcode/BitwiseAndInRange.java
@@ -4,9 +4,11 @@ public class BitwiseAndInRange {
     public static void main(String[] args){
         int low=5;
         int high=7;
-
         computeNaive(low,high);
+        advanceCompute(low,high);
+    }
 
+    private static void advanceCompute(int low, int high) {
     }
 
     private static void computeNaive(int low, int high) {
