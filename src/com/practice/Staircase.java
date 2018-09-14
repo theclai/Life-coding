627ea414d13d36cb1b0cd63bbbf8a073ffdf30f7
nanofaroque <faroque.ae@gmail.com>
2018-09-14 17:40:28 -0500

    advance for stair caser

--- src/com/practice/Staircase.java
+++ src/com/practice/Staircase.java
@@ -1,9 +1,12 @@
 package com.practice;
 
+import java.util.Hashtable;
+
 public class Staircase {
+    static Hashtable<Integer,Integer> table=new Hashtable<>();
     public static void main(String[] args){
         int n=2;
-        int res=climbStairs(2);
+        int res=climbStairs(5);
         System.out.println(res);
     }
 
@@ -13,6 +16,20 @@ public class Staircase {
         else if (n == 2)
             return 2;
 
-        return climbStairs(n-1)+climbStairs(n-2);
+        if(table.contains(n))
+            return table.get(n);
+        int result= climbStairs(n-1)+climbStairs(n-2);
+        table.put(n,result);
+        return table.get(n);
+    }
+
+    public static int climbStoresAdvance(int n){
+        int[] f = new int[n + 1];
+        f[0] = 1;
+        f[1] = 1;
+        for (int i = 2; i <= n; i++){
+            f[i] = f[i - 1] + f[i - 2];
+        }
+        return f[n];
     }
 }
