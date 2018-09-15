6fba1953d1434fd917c778f764548db2370f880e
Omar Faroque <faroque.ae@gmail.com>
2018-09-15 14:30:00 -0500

    remove duplicates from sorted linked list

---
+++ src/leetcode/RemovedulplicateFromSortedList_83.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+public class RemovedulplicateFromSortedList_83 {
+    // 1->1->2->4->3->3->5->5
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode newMember=head;
+        while(newMember!=null){
+            ListNode distinct=newMember.next;
+
+            while (distinct!=null && distinct.val==newMember.val){
+                distinct=distinct.next;
+            }
+            newMember.next=distinct;
+            newMember=distinct;
+
+        }
+        return head;
+
+    }
+}
