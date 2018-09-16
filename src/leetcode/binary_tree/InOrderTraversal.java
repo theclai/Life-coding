1d35aa3f6ad038cb7639983daf3487e71324bc57
Omar Faroque <faroque.ae@gmail.com>
2018-09-16 14:04:09 -0500

    preorder and post order traversal

--- src/leetcode/binary_tree/InOrderTraversal.java
+++ src/leetcode/binary_tree/InOrderTraversal.java
@@ -17,15 +17,42 @@ public class InOrderTraversal {
         root.right=new TreeNode(3);
         root.left.left=new TreeNode(4);
         root.left.right= new TreeNode(5);
+        System.out.println("Inorder Traversal: ");
         inOrderTraversal(root);
+        System.out.println();
 
+        System.out.println("Pre-Order Traversal: ");
+        preOrderTraversal(root);
+        System.out.println();
+
+        System.out.println("Post-Order Traversal: ");
+        postOrderTraversal(root);
+        System.out.println();
+
+    }
+
+    private static void postOrderTraversal(TreeNode root) {
+        if(root==null)
+            return;
+        postOrderTraversal(root.left);
+        postOrderTraversal(root.right);
+        System.out.print(root.val+"->");
     }
 
     public static void inOrderTraversal(TreeNode root){
         if(root==null)
             return;
         inOrderTraversal(root.left);
-        System.out.println(root.val);
+        System.out.print(root.val+"->");
         inOrderTraversal(root.right);
+    }
+
+    public static void preOrderTraversal(TreeNode node){
+        if(node==null)
+            return;
+        System.out.print(node.val+"->");
+        preOrderTraversal(node.left);
+        preOrderTraversal(node.right);
+
     }
 }
