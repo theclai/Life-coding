8008ec7f4bf4dc09d04e535626ab3525273e622f
nanofaroque <faroque.ae@gmail.com>
2018-09-18 16:03:36 -0500

    code cleaning and response clean

--- src/geeksforgeeks/ChainedMatrixMultiplication.java
+++ src/geeksforgeeks/ChainedMatrixMultiplication.java
@@ -1,22 +1,26 @@
 package geeksforgeeks;
 
 public class ChainedMatrixMultiplication {
-    static String dump="";
-    static String beg="";
-    public static void main(String[] args) {
-        String in="abcd";
-        compute(in,0);
+    static String dump = "";
+    static String beg = "";
 
+    public static void main(String[] args) {
+        String in = "abcd";
+        compute(in, 0);
     }
 
-    private static void compute(String in,int pos) {
+    private static void compute(String in, int pos) {
+        if (pos <= in.length() - 1) {
+            if (beg.length() == 0) {
+                dump = "(" + in + ")";
+                System.out.println(dump);
+            } else {
+                dump = "(" + beg + ")" + in;
+                System.out.println(dump);
+            }
 
-        if(pos<=in.length()-1){
-            dump=beg+"->"+in;
-            System.out.println(dump);
-            beg=beg+in.substring(0,pos+1);
-            compute(in.substring(pos+1,in.length()),pos);
+            beg = beg + in.substring(0, pos + 1);
+            compute(in.substring(pos + 1, in.length()), pos);
         }
-
     }
 }
