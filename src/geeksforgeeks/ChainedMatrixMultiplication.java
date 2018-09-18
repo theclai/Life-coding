8f30b7bf347927b300a3901792553358807536f7
nanofaroque <faroque.ae@gmail.com>
2018-09-18 15:56:44 -0500

    fixing bug and handling error

--- src/geeksforgeeks/ChainedMatrixMultiplication.java
+++ src/geeksforgeeks/ChainedMatrixMultiplication.java
@@ -4,15 +4,16 @@ public class ChainedMatrixMultiplication {
     static String dump="";
     static String beg="";
     public static void main(String[] args) {
-        String in="abc";
+        String in="abcd";
         compute(in,0);
 
     }
 
     private static void compute(String in,int pos) {
-        dump=beg+"->"+in;
-        System.out.println(dump);
-        if(pos<=in.length()){
+
+        if(pos<=in.length()-1){
+            dump=beg+"->"+in;
+            System.out.println(dump);
             beg=beg+in.substring(0,pos+1);
             compute(in.substring(pos+1,in.length()),pos);
         }
