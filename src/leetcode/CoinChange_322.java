d5f2f180230a381e33f37258eab84ea3adeae6f0
nanofaroque <faroque.ae@gmail.com>
2018-09-20 15:41:24 -0500

    coin change work but failed to pass all the case

--- src/leetcode/CoinChange_322.java
+++ src/leetcode/CoinChange_322.java
@@ -1,8 +1,29 @@
 package leetcode;
 
+import java.util.Currency;
+
 public class CoinChange_322 {
     public int coinChange(int[] coins, int amount) {
-        return 0;
+        int[] cache = new int[amount + 1];
+
+        /**
+         * We will computing 0 dollars to amount
+         * */
+        for (int i = 1; i <= amount; i++) {
+            int min=Integer.MAX_VALUE;
+            for (int c : coins) {
+                if (i - c >= 0) {
+                    int currCoins = cache[i-c] + 1;
+                    if(currCoins<min){
+                        min=currCoins;
+                    }
+                }
+            }
+            cache[i]=min;
+
+        }
+
+        return cache[amount];
     }
 
 }
--- src/leetcode/CoinChange_322_Test.java
+++ src/leetcode/CoinChange_322_Test.java
@@ -1,11 +1,36 @@
 package leetcode;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class CoinChange_322_Test {
     CoinChange_322 coinChange;
+    int[] coins,coins1;
+    int amt,amt1;
+    int groundTruth,groundTruth1;
     @BeforeEach
     public void init(){
         coinChange=new CoinChange_322();
+        coins=new int[]{1,2,5};
+        amt=11;
+        groundTruth=3;
+
+        coins1=new int[]{2};
+        amt1=3;
+        groundTruth1=-1;
+    }
+
+    @Test
+    public void countTest(){
+        int expected=coinChange.coinChange(coins,amt);
+        Assertions.assertEquals(expected,groundTruth);
+    }
+
+
+    @Test
+    public void countTestSecond(){
+        int expected=coinChange.coinChange(coins1,amt1);
+        Assertions.assertEquals(expected,groundTruth1);
     }
 }
