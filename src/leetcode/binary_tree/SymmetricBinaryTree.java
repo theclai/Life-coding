ce1b666ad1cce125e66d3f084a32cc2eb096715c
Omar Faroque <faroque.ae@gmail.com>
2018-09-20 08:21:18 -0500

    symmetric binary tree

---
+++ src/leetcode/binary_tree/SymmetricBinaryTree.java
@@ -0,0 +1,27 @@
+package leetcode.binary_tree;
+
+
+public class SymmetricBinaryTree {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(4);
+        isSymmetric(root);
+    }
+
+    private static boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return helper(root.left, root.right);
+    }
+
+    private static boolean helper(TreeNode left, TreeNode right) {
+        if(left==null && right==null){
+            return true;
+        }else if(left!=null && right!=null){
+            return (left.val==right.val)&&
+                    helper(left.left, right.right) &&
+                    helper(left.right, right.left);
+        }
+        return false;
+    }
+}
