8a5191147ea3c8010c14aad17d313fd2cd0c7b24
nanofaroque <faroque.ae@gmail.com>
2018-09-20 15:05:08 -0500

    coin chnage setup for bottom up approach

---
+++ src/leetcode/CoinChange_322.java
@@ -0,0 +1,8 @@
+package leetcode;
+
+public class CoinChange_322 {
+    public int coinChange(int[] coins, int amount) {
+        return 0;
+    }
+
+}
---
+++ src/leetcode/CoinChange_322_Test.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class CoinChange_322_Test {
+    CoinChange_322 coinChange;
+    @BeforeEach
+    public void init(){
+        coinChange=new CoinChange_322();
+    }
+}
