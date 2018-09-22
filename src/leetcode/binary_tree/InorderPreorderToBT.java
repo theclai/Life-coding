3f71b9f8d3ee0b6effcaace081e903c7ca0eea28
Omar Faroque <faroque.ae@gmail.com>
2018-09-22 17:51:03 -0500

    inorder pre order to binary tree

--- src/leetcode/binary_tree/InorderPreorderToBT.java
+++ src/leetcode/binary_tree/InorderPreorderToBT.java
@@ -1,6 +1,7 @@
 package leetcode.binary_tree;
 
 public class InorderPreorderToBT {
+    static int preIndex=0;
     public static void main(String[] args) {
         char[] in = new char[]{'D', 'B', 'E', 'A', 'F', 'C'};
         char[] pre = new char[]{'A', 'B', 'D', 'E', 'C', 'F'};
@@ -8,15 +9,28 @@ public class InorderPreorderToBT {
         buildTree(in, pre);
     }
 
-    private static void buildTree(char[] in, char[] pre) {
-         buildHelper(in,pre,0,in.length-1);
+    private static TreeNode buildTree(char[] in, char[] pre) {
+        return buildHelper(in, pre, 0, in.length - 1);
     }
 
-    private static void buildHelper(char[] in,char[] pre,int start,int end) {
-        char val=pre[0];
-        TreeNode root=new TreeNode(val);
-        root.left=search(in,start,end,val);
-        root.
+    private static TreeNode buildHelper(char[] in,
+                                        char[] pre,
+                                        int start,
+                                        int end) {
+        if (end < start) {
+            return null;
+        }
+
+        TreeNode root=new TreeNode(pre[preIndex++]);
+        if (start == end) {
+            return root;
+        }
+
+        int searchOutput = search(in, start, end, root.data);
+        root.left = buildHelper(in, pre, start, searchOutput - 1);
+        root.right = buildHelper(in, pre, searchOutput + 1, end);
+        return root;
+
     }
 
     static int search(char arr[], int strt, int end, char value) {
--- src/tree/TreeNode.java
+++ src/tree/TreeNode.java
@@ -4,8 +4,13 @@ public class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
+    char data;
 
     TreeNode(int x) {
         val = x;
+    }
+
+    TreeNode(char x) {
+        data = x;
     }
 }
