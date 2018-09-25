25266723a8d6dddf519222b2ae10875abccce2b1
nanofaroque <faroque.ae@gmail.com>
2018-09-25 15:53:26 -0500

    matrix product advance bottom up approach

--- src/com/practice/MaxProductInMatrix.java
+++ src/com/practice/MaxProductInMatrix.java
@@ -3,13 +3,30 @@ package com.practice;
 public class MaxProductInMatrix {
     public static void main(String[] args) {
         int[][] matrix = new int[][]{
-                {1,  2,  3},
-                { 4,  5, 6},
-                { 7,  8,  9}
+                {-1, 2, 3},
+                {4, 5, -6},
+                {7, 8, 9}
         };
         int row = matrix.length;
         int col = matrix[0].length;
         System.out.println(maxProduct(matrix, row - 1, col - 1));
+        System.out.println(maxProductAdvance(matrix));
+    }
+
+    private static int maxProductAdvance(int[][] matrix) {
+        // We need two caches to process negative number.
+        int[][] minCache = new int[matrix.length][matrix[0].length];
+        int[][] maxCache = new int[matrix.length][matrix[0].length];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (i == 0 && j == 0) {
+                    minCache[i][j] = matrix[i][j];
+                    maxCache[i][j] = matrix[i][j];
+                }
+            }
+        }
+        return maxCache[matrix.length][matrix[0].length];
     }
 
     private static int maxProduct(int[][] matrix, int row, int col) {
