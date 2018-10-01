4e2d63e39e8b6438b2a575e5c6b7fca9ca710ee6
Omar Faroque <faroque.ae@gmail.com>
2018-09-30 22:04:48 -0500

    finishing minimal tree from sorted array

--- src/cracking_coding_int/MinimalTree.java
+++ src/cracking_coding_int/MinimalTree.java
@@ -7,16 +7,24 @@ import leetcode.TreeNode;
  * */
 public class MinimalTree {
     public static void main(String[] args) {
-        int[] array=new int[]{1,2,3,4,5,6,7,8,9};
+        int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
         // To make minimal height, we have to add half element in the left subtree and right half on the right subtree
-        createminimalTree(array);
+        TreeNode tree = createminimalTree(array);
+        System.out.println(tree.val);
     }
 
     private static TreeNode createminimalTree(int[] array) {
-        return helper(array,0,array.length-1);
+        return helper(array, 0, array.length - 1);
     }
 
     private static TreeNode helper(int[] array, int start, int end) {
-
+        if (start > end) {
+            return null;
+        }
+        int mid = (start + end) / 2;
+        TreeNode root = new TreeNode(array[mid]);
+        root.left = helper(array, start, mid - 1);
+        root.right = helper(array, mid + 1, end);
+        return root;
     }
 }
