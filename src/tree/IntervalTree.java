0b3e7f74f895f4414ad3aace297bccbaef1b4730
Omar Faroque <faroque.ae@gmail.com>
2018-10-01 21:48:25 -0500

    overlaping detection

--- src/tree/IntervalTree.java
+++ src/tree/IntervalTree.java
@@ -68,19 +68,30 @@ public class IntervalTree {
 
     private static IntrvalTree insert(IntrvalTree root,
                                       int[] interval) {
-        if(root==null){
-            root=new IntrvalTree(interval[0],interval[1]);
+        if (root == null) {
+            root = new IntrvalTree(interval[0], interval[1]);
             root.setMax(interval[1]);
             return root;
         }
-        int high=interval[1];
-        if(high>=root.getMax()){
-            root.right=insert(root.right,interval);
-        }else {
-            root.left=insert(root.left,interval);
+        int high = interval[1];
+        if (high >= root.getMax()) {
+            root.right = insert(root.right, interval);
+        } else {
+            root.left = insert(root.left, interval);
         }
         if (root.max < high)
             root.max = high;
         return root;
+    }
+
+    private static boolean doOVerlap(int[] interval1, int[] interval2) {
+        int i1Low=interval1[0];
+        int i1High=interval1[1];
+
+        int i2Low=interval2[0];
+        int i2High=interval2[1];
+        if (i1Low < i2High && i2Low < i1High)
+            return true;
+        return false;
     }
 }
