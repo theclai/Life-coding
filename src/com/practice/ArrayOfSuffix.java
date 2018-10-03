eaec0926703fc2581126e359ddede8da1a03bdc3
Omar Faroque <faroque.ae@gmail.com>
2018-10-02 20:57:32 -0500

    array of suffix

---
+++ src/com/practice/ArrayOfSuffix.java
@@ -0,0 +1,20 @@
+package com.practice;
+
+public class ArrayOfSuffix {
+    public static void main(String[] args) {
+        String input="Love";
+        String[] suffixes=findSuffix(input);
+        for (int i=0;i<suffixes.length;i++){
+            System.out.println(suffixes[i]);
+        }
+    }
+
+    private static String[] findSuffix(String input) {
+        int N=input.length();
+        String[] suffs= new String[N];
+        for(int i=0;i<input.length();i++){
+            suffs[i]=input.substring(i,N);
+        }
+        return suffs;
+    }
+}
