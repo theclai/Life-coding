fc4a4fc2cb83a2909faf6ffa7c962072a0e4ca1e
Omar Faroque <faroque.ae@gmail.com>
2018-10-02 20:49:09 -0500

    reverse string by using string builder

---
+++ src/com/practice/ReverseString.java
@@ -0,0 +1,17 @@
+package com.practice;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String input="Bangla";
+        System.out.println(reverString(input));
+    }
+
+    private static String reverString(String input) {
+        // String build is immutable and faster
+        StringBuilder builder=new StringBuilder();
+        for (int i = input.length()-1; i >=0 ; i--) {
+            builder.append(input.charAt(i));
+        }
+        return builder.toString();
+    }
+}
