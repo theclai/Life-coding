f42fbb882cc42cf3d7dcb262f47555f33fce2cb6
nanofaroque <faroque.ae@gmail.com>
2018-10-05 10:14:41 -0700

    checking github

--- src/strings/SuffixTreeOrTrieForPatternMatching.java
+++ src/strings/SuffixTreeOrTrieForPatternMatching.java
@@ -3,31 +3,34 @@ package strings;
 import java.util.HashMap;
 
 public class SuffixTreeOrTrieForPatternMatching {
-    private class SuffixTrie {
-        char key;
-        public HashMap<Character, SuffixTrie> map;
+    public static void main(String[] args) {
+        SuffixTree root=new SuffixTree('*');
+        insert("amar",root);
+    }
 
-        SuffixTrie(Character c) {
+    private static class SuffixTree {
+        char key;
+        public HashMap<Character, SuffixTree> map;
+
+        SuffixTree(Character c) {
             this.key = c;
             map = new HashMap<>();
         }
     }
 
 
-    public void insert(String data, SuffixTrie root) {
+    public static void insert(String data, SuffixTree root) {
         char[] chars = data.toCharArray();
-        HashMap<Character, SuffixTrie> map = root.map;
+        HashMap<Character, SuffixTree> map = root.map;
         for (int i = 0; i < chars.length; i++) {
             char c = chars[i];
             if (map.containsKey(c)) {
-                root = map.get(c);
-                map = root.map;
+                map = map.get(c).map;
+                //map = root.map;
             } else {
-                root.map.put(c, new SuffixTrie(c));
+                map.put(c, new SuffixTree(c));
+
             }
-
         }
-
-
     }
 }
