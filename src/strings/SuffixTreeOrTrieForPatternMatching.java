75c395be5ff2b80cd6d1e9b0b6f2866e6ea3219d
nanofaroque <faroque.ae@gmail.com>
2018-10-05 15:37:55 -0700

    search functionalities in trie

--- src/strings/SuffixTreeOrTrieForPatternMatching.java
+++ src/strings/SuffixTreeOrTrieForPatternMatching.java
@@ -6,23 +6,26 @@ public class SuffixTreeOrTrieForPatternMatching {
     public static void main(String[] args) {
         SuffixTree root = new SuffixTree();
         insert("amar", root);
+        System.out.println(search("amarf", root));
     }
 
     private static class SuffixTree {
         char key;
         private HashMap<Character, SuffixTree> map;
         private boolean isLeaf;
+
         SuffixTree(Character c) {
             this.key = c;
             this.map = new HashMap<>();
         }
-        SuffixTree(){
-            this.map=new HashMap<>();
+
+        SuffixTree() {
+            this.map = new HashMap<>();
         }
     }
 
 
-    public static SuffixTree insert(String data, SuffixTree root) {
+    static SuffixTree insert(String data, SuffixTree root) {
         char[] chars = data.toCharArray();
         HashMap<Character, SuffixTree> map = root.map;
         for (int i = 0; i < chars.length; i++) {
@@ -38,10 +41,36 @@ public class SuffixTreeOrTrieForPatternMatching {
             map = st.map;
 
             //set leaf node
-            if(i==data.length()-1)
+            if (i == data.length() - 1)
                 st.isLeaf = true;
         }
 
         return root;
+    }
+
+    public static boolean search(String search, SuffixTree root) {
+        SuffixTree searchNode = searchWord(search, root);
+        if (searchNode != null && searchNode.isLeaf)
+            return true;
+        else
+            return false;
+    }
+
+    private static SuffixTree searchWord(String search, SuffixTree root) {
+        SuffixTree st=null;
+        for (int i = 0; i < search.length(); i++) {
+            char c=search.charAt(i);
+            HashMap<Character,SuffixTree> map=root.map;
+            if(map.containsKey(c)){
+                st=map.get(c);
+                if(st==null)
+                    return st;
+                root=st;
+
+            }else {
+                return null;
+            }
+        }
+        return st;
     }
 }
