49a56a5da0d9dc10183d3e3dc1cd9abd724e91b7
nanofaroque <faroque.ae@gmail.com>
2018-10-05 15:26:19 -0700

    fixing suffix tree insert method

--- src/strings/SuffixTreeOrTrieForPatternMatching.java
+++ src/strings/SuffixTreeOrTrieForPatternMatching.java
@@ -4,33 +4,44 @@ import java.util.HashMap;
 
 public class SuffixTreeOrTrieForPatternMatching {
     public static void main(String[] args) {
-        SuffixTree root=new SuffixTree('*');
-        insert("amar",root);
+        SuffixTree root = new SuffixTree();
+        insert("amar", root);
     }
 
     private static class SuffixTree {
         char key;
-        public HashMap<Character, SuffixTree> map;
-
+        private HashMap<Character, SuffixTree> map;
+        private boolean isLeaf;
         SuffixTree(Character c) {
             this.key = c;
-            map = new HashMap<>();
+            this.map = new HashMap<>();
+        }
+        SuffixTree(){
+            this.map=new HashMap<>();
         }
     }
 
 
-    public static void insert(String data, SuffixTree root) {
+    public static SuffixTree insert(String data, SuffixTree root) {
         char[] chars = data.toCharArray();
         HashMap<Character, SuffixTree> map = root.map;
         for (int i = 0; i < chars.length; i++) {
             char c = chars[i];
-            if (map.containsKey(c)) {
-                map = map.get(c).map;
-                //map = root.map;
-            } else {
-                map.put(c, new SuffixTree(c));
 
+            SuffixTree st;
+            if (map.containsKey(c)) {
+                st = map.get(c);
+            } else {
+                st = new SuffixTree(c);
+                map.put(c, st);
             }
+            map = st.map;
+
+            //set leaf node
+            if(i==data.length()-1)
+                st.isLeaf = true;
         }
+
+        return root;
     }
 }
