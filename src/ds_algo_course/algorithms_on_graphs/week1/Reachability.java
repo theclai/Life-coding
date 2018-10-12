9298ba663d1b3294ad552bdf9cfb52531ad90052
nanofaroque <faroque.ae@gmail.com>
2018-10-12 01:18:51 -0500

    reachability solution

--- src/ds_algo_course/algorithms_on_graphs/week1/Reachability.java
+++ src/ds_algo_course/algorithms_on_graphs/week1/Reachability.java
@@ -2,10 +2,34 @@ package ds_algo_course.algorithms_on_graphs.week1;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Reachability {
     private static int reach(ArrayList<Integer>[] adj, int x, int y) {
         //write your code here
+        boolean[] visited=new boolean[adj.length];
+        Stack<Integer> stack=new Stack<>();
+        return dfs(adj,x,y,visited,stack);
+        //return 0;
+    }
+
+    private static int dfs(ArrayList<Integer>[] adj,
+                           int src,
+                           int dst,
+                           boolean[] visited,
+                           Stack<Integer> stack) {
+        stack.add(src);
+        while (!stack.isEmpty()){
+            int node=stack.peek();
+            if (node==dst)
+                return 1;
+            visited[node]=true;
+            for(int i=0;i<adj[i].size();i++){
+                if(!visited[adj[node].get(i)]){
+                    stack.add(adj[node].get(i));
+                }
+            }
+        }
         return 0;
     }
 
