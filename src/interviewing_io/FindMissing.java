b3de1c32854f82502dbc60ec798a907a7a231a31
nanofaroque <faroque.ae@gmail.com>
2018-10-13 15:53:22 -0500

    partially overflow exist

--- src/interviewing_io/FindMissing.java
+++ src/interviewing_io/FindMissing.java
@@ -2,24 +2,45 @@ package interviewing_io;
 
 public class FindMissing {
     public static void main(String[] args) {
-        int[] a=new int[]{4,8,12,9,3};
-        int[] b=new int[]{4,8,9,3};
+        int[] a = new int[]{4, 8, 12, 9, 3};
+        int[] b = new int[]{4, 8, 9, 3};
 
-        System.out.println(findMissing(a,b));
+        System.out.println(findMissing(a, b));
+        System.out.println(findMissingWithoutOverflow(a, b));
     }
 
     private static int findMissing(int[] a, int[] b) {
-        int sum1=0;
-        int sum2=0;
-        for (int i = 0; i < a.length-1; i++) {
-            if(i==b.length-1){
-                sum2=sum2+0;
-                sum1=sum1+a[i];
-            }else{
-                sum1=sum1+a[i];
-                sum2=sum2+b[i];
+        int sum1 = 0;
+        int sum2 = 0;
+        for (int i = 0; i < a.length - 1; i++) {
+            if (i == b.length - 1) {
+                sum2 = sum2 + 0;
+                sum1 = sum1 + a[i];
+            } else {
+                sum1 = sum1 + a[i];
+                sum2 = sum2 + b[i];
             }
         }
-        return sum1-sum2;
+        return sum1 - sum2;
+    }
+
+
+    private static int findMissingWithoutOverflow(int[] bigger,
+                                                  int[] smaller) {
+        int sum=0;
+        int h1=0;
+        int h2=0;
+        while(h1<bigger.length && h2<smaller.length){
+            sum+=bigger[h1];
+            sum-=smaller[h2];
+            h1++;
+            h2++;
+        }
+        if(h1<bigger.length){
+            sum+=bigger[h1];
+        }
+        if(h2<smaller.length)
+            sum-=smaller[h2];
+        return sum;
     }
 }
