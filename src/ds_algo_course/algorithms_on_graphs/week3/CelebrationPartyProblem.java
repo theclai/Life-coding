846e5dc7f6ffa221eeec4d650f486f7305c624d4
nanofaroque <faroque.ae@gmail.com>
2018-10-16 16:38:52 -0500

    grouping kids based on ages for minimum teacher

---
+++ src/ds_algo_course/algorithms_on_graphs/week3/CelebrationPartyProblem.java
@@ -0,0 +1,27 @@
+package ds_algo_course.algorithms_on_graphs.week3;
+
+public class CelebrationPartyProblem {
+    public static void main(String[] args) {
+        // Just assume data is sorted, if not just sort it out
+        double[] ages = new double[]{2.5, 3.2, 3.5, 4.0, 4.8, 5.2, 6.0};
+        double min = ages[0];
+        double max = ages[ages.length - 1];
+
+        System.out.println(minimumTeacherRequired(ages, min, max));
+    }
+
+    private static int minimumTeacherRequired(double[] ages, double min, double max) {
+        int teacher_count = 0;
+        int i=0;
+        while(i<=ages.length-1){
+            if(ages[i]<=min+1){
+                // make it as a group
+            }else{
+                teacher_count++;
+                min=ages[i];
+            }
+            i++;
+        }
+        return teacher_count+1;//
+    }
+}
