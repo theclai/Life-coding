071c033dedb0af7a8e66362bafe6ecd477d23cd1
nanofaroque <faroque.ae@gmail.com>
2018-10-16 16:42:48 -0500

    quick fix

--- src/ds_algo_course/algorithms_on_graphs/week3/CelebrationPartyProblem.java
+++ src/ds_algo_course/algorithms_on_graphs/week3/CelebrationPartyProblem.java
@@ -6,22 +6,20 @@ public class CelebrationPartyProblem {
         double[] ages = new double[]{2.5, 3.2, 3.5, 4.0, 4.8, 5.2, 6.0};
         double min = ages[0];
         double max = ages[ages.length - 1];
-
         System.out.println(minimumTeacherRequired(ages, min, max));
     }
 
     private static int minimumTeacherRequired(double[] ages, double min, double max) {
         int teacher_count = 0;
-        int i=0;
-        while(i<=ages.length-1){
-            if(ages[i]<=min+1){
+        int i = 0;
+        while (i <= ages.length - 1) {
+            if (ages[i] > min + 1) {
                 // make it as a group
-            }else{
                 teacher_count++;
-                min=ages[i];
+                min = ages[i];
             }
             i++;
         }
-        return teacher_count+1;//
+        return teacher_count + 1;//
     }
 }
