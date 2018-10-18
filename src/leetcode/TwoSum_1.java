af2b8887724b4737fa91467ff15843cae67466fc
Omar Faroque <faroque.ae@gmail.com>
2018-10-17 19:12:48 -0500

    two sum problem

---
+++ src/leetcode/TwoSum_1.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+public class TwoSum_1 {
+    public static void main(String[] args) {
+        //int[] nums = new int[]{3,2,4};
+        //int target = 6;
+
+        int[] nums = new int[]{2, 7, 11, 15};
+        int target = 9;
+
+        int[] result=findIndices(nums,target);
+        System.out.println(result[0]);
+        System.out.println(result[1]);
+    }
+
+    private static int[] findIndices(int[] nums, int target) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int diff=target-nums[i];
+            if(map.containsKey(nums[i]))
+                return new int[]{map.get(nums[i]),i};
+            else {
+                map.put(diff,i);
+            }
+        }
+        return new int[]{0,0};
+    }
+}
