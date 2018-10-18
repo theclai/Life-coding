38313e20d5c6d5f516a462b8b9d86a3831dea92c
Omar Faroque <faroque.ae@gmail.com>
2018-10-17 19:13:23 -0500

    code cleaning

--- src/leetcode/TwoSum_1.java
+++ src/leetcode/TwoSum_1.java
@@ -1,9 +1,6 @@
 package leetcode;
 
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
 
 public class TwoSum_1 {
     public static void main(String[] args) {
@@ -12,22 +9,21 @@ public class TwoSum_1 {
 
         int[] nums = new int[]{2, 7, 11, 15};
         int target = 9;
-
-        int[] result=findIndices(nums,target);
+        int[] result = findIndices(nums, target);
         System.out.println(result[0]);
         System.out.println(result[1]);
     }
 
     private static int[] findIndices(int[] nums, int target) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            int diff=target-nums[i];
-            if(map.containsKey(nums[i]))
-                return new int[]{map.get(nums[i]),i};
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if (map.containsKey(nums[i]))
+                return new int[]{map.get(nums[i]), i};
             else {
-                map.put(diff,i);
+                map.put(diff, i);
             }
         }
-        return new int[]{0,0};
+        return new int[]{0, 0};
     }
 }
