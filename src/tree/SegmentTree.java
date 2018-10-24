d85a8408cf2f2d6f7a1a3445c0be7063fce5d2aa
nanofaroque <faroque.ae@gmail.com>
2018-10-24 17:25:54 -0500

    code cleaning

--- src/tree/SegmentTree.java
+++ src/tree/SegmentTree.java
@@ -1,5 +1,7 @@
 package tree;
 
+import java.util.Arrays;
+
 /**
  * 1. ST is a binary tree. In a tree data stored as
  * parent = i
@@ -14,7 +16,28 @@ package tree;
  * 5. compute the parents and put into right position based on step-1
  */
 public class SegmentTree {
+    static int[] tree;
+
+    public SegmentTree(int[] a) {
+        int n = a.length;
+        tree = new int[2 * n];
+        // insert leaf nodes in tree
+        for (int i = 0; i < n; i++)
+            tree[n + i] = a[i];
+
+        // build the tree by calculating
+        // parents
+        for (int i = n - 1; i > 0; --i)
+            tree[i] = tree[i << 1] + // left shift means 2*i
+                    tree[i << 1 | 1];// left shit and add 1. 2*i+1
+        Arrays.stream(tree).forEach(System.out::println);
+    }
+
     public static void main(String[] args) {
-        SegmentTree st = new SegmentTree(new int[]{1, 3, 5, 7, 9, 11}, 6);
+        int[] a = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+        //SegmentTree st = new SegmentTree(a);
+        System.out.println(a.length);
+        System.out.println(a.length << 1 | 1);
+
     }
 }
