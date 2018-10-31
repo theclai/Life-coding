518ed27b27a69c396beae2c39da3b3fcee7dbba4
Omar Faroque <faroque.ae@gmail.com>
2018-10-30 22:04:53 -0500

    power compute

---
+++ src/interviewbit/PowFunction.java
@@ -0,0 +1,27 @@
+package interviewbit;
+
+import java.math.BigInteger;
+
+public class PowFunction {
+    public int power(int x, int n, int d) {
+        int pow = 1;
+        // do till n is not zero
+        while (n > 0) {
+            // if n is odd, multiply result by x
+            if ((n & 1) == 1) {
+                pow *= x;
+            }
+            // divide n by 2
+            n = n >> 1;
+            // multiply x by itself
+            x = x * x;
+        }
+        // return result
+        return pow;
+    }
+
+    public static void main(String[] args) {
+        PowFunction pf = new PowFunction();
+        System.out.println(pf.power(2, 5, 20));
+    }
+}
