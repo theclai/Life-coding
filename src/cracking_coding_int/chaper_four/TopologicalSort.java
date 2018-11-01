50ff36e594af790d084d3394a99c16d7e8e18b54
nanofaroque <faroque.ae@gmail.com>
2018-11-01 09:53:30 -0500

    finishing topological sort

--- src/cracking_coding_int/chaper_four/TopologicalSort.java
+++ src/cracking_coding_int/chaper_four/TopologicalSort.java
@@ -2,10 +2,15 @@ package cracking_coding_int.chaper_four;
 
 import graph.Graph;
 
+import java.util.Iterator;
+import java.util.Stack;
+
 public class TopologicalSort {
+    static Graph g;
+
     public static void main(String[] args) {
-        int V=6;
-        Graph g = new Graph(6);
+        int V = 6;
+        g = new Graph(6);
         g.addEdge(5, 2);
         g.addEdge(5, 0);
         g.addEdge(4, 0);
@@ -15,10 +20,33 @@ public class TopologicalSort {
 
         System.out.println("Following is a Topological " +
                 "sort of the given graph");
-        topologicalSort(g,V);
+        topologicalSort(g, V);
     }
 
     private static void topologicalSort(Graph g, int v) {
+        Stack<Integer> stack = new Stack<>();
+        boolean[] visited = new boolean[v];
+        for (int i = 0; i < v; i++)
+            visited[i] = false;
+        for (int i = 0; i < v; i++)
+            if (visited[i] == false)
+                topologicalSortUtil(i, visited, stack);
 
+        // Print contents of stack
+        while (stack.empty() == false)
+            System.out.print(stack.pop() + " ");
+
+    }
+
+    private static void topologicalSortUtil(int v, boolean[] visited, Stack<Integer> stack) {
+        visited[v] = true;
+        Iterator<Integer> it = g.adj[v].iterator();
+        Integer i;
+        while (it.hasNext()) {
+            i = it.next();
+            if (!visited[i])
+                topologicalSortUtil(i, visited, stack);
+        }
+        stack.push(new Integer(v));
     }
 }
