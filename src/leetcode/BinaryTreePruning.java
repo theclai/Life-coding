0cf824829cc228328e6c2b94f58a6191fd09b78a
nanofaroque <faroque.ae@gmail.com>
2018-11-05 11:18:25 -0600

    prune tree advance

--- src/leetcode/BinaryTreePruning.java
+++ src/leetcode/BinaryTreePruning.java
@@ -8,8 +8,11 @@ public class BinaryTreePruning {
         root.right.left = new TreeNode(0);
         root.right.right = new TreeNode(1);
         BinaryTreePruning btp = new BinaryTreePruning();
-        TreeNode r=btp.pruneTree(root);
-        System.out.println(r.val);
+        //TreeNode r=btp.pruneTree(root);
+        //System.out.println(r.val);
+        System.out.println(btp.pruneTreeEdge(root).val);
+
+        System.out.println(8 == 5 || true || false);
     }
 
     public TreeNode pruneTree(TreeNode root) {
@@ -23,4 +26,22 @@ public class BinaryTreePruning {
         }
         return root;
     }
+
+
+    public TreeNode pruneTreeEdge(TreeNode root) {
+        return containsOne(root) ? root : null;
+    }
+
+    private boolean containsOne(TreeNode root) {
+        if (root == null) return false;
+        boolean left = containsOne(root.left);
+        boolean right = containsOne(root.right);
+        if (!left) root.left = null;
+        if (!right) root.right = null;
+        return root.val == 1 || left || right;// any of the three statements is true will return true
+
+
+    }
+
+
 }
