73c3f30b3a4c7b10f52f16725c346ab73105c289
Omar Faroque <faroque.ae@gmail.com>
2018-11-05 19:29:31 -0600

    dp solution

--- src/coderbyte/FindSubArray.java
+++ src/coderbyte/FindSubArray.java
@@ -18,4 +18,29 @@ public class FindSubArray {
                     insert(in,s,pos+1);
 
     }
+
+    public static boolean subSetDP(int[] A, int sum) {
+        boolean[][] solution = new boolean[A.length + 1][sum + 1];
+        // if sum is not zero and subset is 0, we can't make it
+        for(int i=1;i<=sum;i++){
+            solution[0][i]=false;
+        }
+        // if sum is 0 the we can make the empty subset to make sum 0
+        for(int i=0;i<=A.length;i++){
+            solution[i][0]=true;
+        }
+        //
+        for(int i=1;i<=A.length;i++){
+            for(int j=1;j<=sum;j++){
+                //first copy the data from the top
+                solution[i][j] = solution[i-1][j];
+
+                //If solution[i][j]==false check if can be made
+                if(solution[i][j]==false && j>=A[i-1])
+                    solution[i][j] = solution[i][j] || solution[i-1][j-A[i-1]];
+            }
+        }
+        return solution[A.length][sum];
+    }
+
 }
