40e9ea72e4e6cad491a590c1b62a7cee0d705c8d
Omar Faroque <faroque.ae@gmail.com>
2018-11-22 09:47:50 -0600

    advance for array quadruplet

--- src/pramp/ArrayQuadruplet.java
+++ src/pramp/ArrayQuadruplet.java
@@ -37,19 +37,46 @@ public class ArrayQuadruplet {
                         int tempSum = arr[i] + arr[j] + arr[k] + arr[l];
                         if (tempSum == s)
                             result = new int[]{arr[i], arr[j], arr[k], arr[l]};
-                        }
-
                     }
+
                 }
             }
-        return result;
         }
-
+        return result;
+    }
 
 
     public static void main(String[] args) {
         int[] input = new int[]{2, 7, 4, 0, 9, 5, 1, 3};
+        Arrays.sort(input);
         int[] res = findArrayQuadruplet(input, 20);
         Arrays.sort(res);
+
+        int[] ad= findQuad(input, 20);
+        Arrays.stream(ad).forEach(i->System.out.print(i+"->"));
+    }
+
+    private static int[] findQuad(int[] in, int s) {
+        int len = in.length;
+        if (len < 4)
+            return new int[0];
+        for (int i = 0; i < len - 4; i++) {
+            for (int j = 0; j < len - 3; j++) {
+                int r = s - (in[i] + in[j]);
+                int low = j + 1;
+                int high = len - 1;
+
+                while (low < high) {
+                    if(in[low]+in[high]<r)
+                        low++;
+                    else if(in[low]+in[high]<r)
+                        high--;
+                    else
+                        return new int[]{in[i],in[j],in[low],in[high]};
+                }
+
+            }
+        }
+        return new int[0];
     }
 }
