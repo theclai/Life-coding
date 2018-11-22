ebddd8d007623e2386b9e80afef4367f33fadb57
Omar Faroque <faroque.ae@gmail.com>
2018-11-22 09:00:08 -0600

    arrays quadruplet

---
+++ src/pramp/ArrayQuadruplet.java
@@ -0,0 +1,55 @@
+package pramp;
+
+import java.util.Arrays;
+
+public class ArrayQuadruplet {
+    /**
+     * unsorted array
+     * random quadruplet
+     * <p>
+     * 0 1 2 3  4 5 7 9
+     * <p>
+     * i=0;
+     * j=7
+     * <p>
+     * s = 20 .. s=11 j--  j=6 counter = 1
+     * s = 20 .. s=4 j--  j=5 counter = 2
+     * <p>
+     * <p>
+     * <p>
+     * <p>
+     * <p>
+     * while(i<j){
+     * <p>
+     * }
+     */
+
+    static int[] findArrayQuadruplet(int[] arr, int s) {
+        if (arr.length < 4)
+            return new int[0];
+
+        int[] result = new int[4];
+        // your code goes here
+        for (int i = 0; i < arr.length - 3; i++) {
+            for (int j = i + 1; j < arr.length - 2; j++) {
+                for (int k = j + 1; k < arr.length - 1; k++) {
+                    for (int l = k + 1; l < arr.length; l++) {
+                        int tempSum = arr[i] + arr[j] + arr[k] + arr[l];
+                        if (tempSum == s)
+                            result = new int[]{arr[i], arr[j], arr[k], arr[l]};
+                        }
+
+                    }
+                }
+            }
+        return result;
+        }
+
+
+
+    public static void main(String[] args) {
+        int[] input = new int[]{2, 7, 4, 0, 9, 5, 1, 3};
+        int[] res = findArrayQuadruplet(input, 20);
+        Arrays.sort(res);
+    }
+}
