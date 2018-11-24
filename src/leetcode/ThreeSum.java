ac08345a2e27eca6708c8a1ccaca4b648ce133c0
Omar Faroque <faroque.ae@gmail.com>
2018-11-24 08:28:02 -0600

    three sum fix

--- src/leetcode/ThreeSum.java
+++ src/leetcode/ThreeSum.java
@@ -8,13 +8,29 @@ public class ThreeSum {
     public static final int TARGET = 0;
 
     public static void main(String[] args) {
-        int[] input = new int[]{-1, 0, 1, 2, -1, -4};
+        //int[] input = new int[]{-1, 0, 1, 2, -1, -4};
+        int[] input = new int[]{0,0,0};
         List<List<Integer>> threeSum = findThreeSum(input);
         System.out.println(threeSum.size());
     }
 
     private static List<List<Integer>> findThreeSum(int[] input) {
         Set<List<Integer>> op = new HashSet<>();
+        if(input.length==3){
+            int tSum=input[0]+input[1]+input[2];
+            if(tSum==0){
+                List<Integer> l = new ArrayList<>();
+                l.add(input[0]);
+                l.add(input[1]);
+                l.add(input[2]);
+                Collections.sort(l);
+                op.add(l);
+            }
+            List<List<Integer>> res=new ArrayList<>();
+            res.addAll(op);
+            return res ;
+        }
+
         for (int i = 0; i < input.length - 1; i++) {
             for (int j = i + 1; j < input.length - 2; j++) {
                 int twoSum = input[i] + input[j];
