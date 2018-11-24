7c2e57026ebc8a02a0945af95b4cca8bcf27c2a1
Omar Faroque <faroque.ae@gmail.com>
2018-11-24 08:38:22 -0600

    three sum problem

--- src/leetcode/ThreeSum.java
+++ src/leetcode/ThreeSum.java
@@ -9,16 +9,16 @@ public class ThreeSum {
 
     public static void main(String[] args) {
         //int[] input = new int[]{-1, 0, 1, 2, -1, -4};
-        int[] input = new int[]{0,0,0};
+        int[] input = new int[]{0, 0, 0};
         List<List<Integer>> threeSum = findThreeSum(input);
         System.out.println(threeSum.size());
     }
 
     private static List<List<Integer>> findThreeSum(int[] input) {
         Set<List<Integer>> op = new HashSet<>();
-        if(input.length==3){
-            int tSum=input[0]+input[1]+input[2];
-            if(tSum==0){
+        if (input.length == 3) {
+            int tSum = input[0] + input[1] + input[2];
+            if (tSum == 0) {
                 List<Integer> l = new ArrayList<>();
                 l.add(input[0]);
                 l.add(input[1]);
@@ -26,9 +26,9 @@ public class ThreeSum {
                 Collections.sort(l);
                 op.add(l);
             }
-            List<List<Integer>> res=new ArrayList<>();
+            List<List<Integer>> res = new ArrayList<>();
             res.addAll(op);
-            return res ;
+            return res;
         }
 
         for (int i = 0; i < input.length - 1; i++) {
@@ -48,8 +48,40 @@ public class ThreeSum {
                 }
             }
         }
-        List<List<Integer>> res=new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
         res.addAll(op);
-        return res ;
+        return res;
+    }
+
+
+    public static List<List<Integer>> findThreeSumBetter(int[] num) {
+        Arrays.sort(num);
+        LinkedList<List<Integer>> ret = new LinkedList<>();
+        HashSet<List<Integer>> set = new HashSet<>();
+
+        for (int i = 0; i < num.length - 2; i++) {
+            int start = i + 1;
+            int end = num.length - 1;
+
+            while (start < end) {
+                if (num[i] + num[start] + num[end] == 0) {
+                    LinkedList<Integer> oneResult = new LinkedList<>();
+                    oneResult.add(num[i]);
+                    oneResult.add(num[start]);
+                    oneResult.add(num[end]);
+                    set.add(oneResult);
+                    start++;
+                    end--;
+                } else {
+                    if (num[i] + num[start] + num[end] < 0)
+                        start++;
+                    else
+                        end--;
+                }
+            }
+        }
+        ret.addAll(set);
+        return ret;
+
     }
 }
