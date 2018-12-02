794763488aa320f3312e7688eeac58da6553603f
Omar Faroque <faroque.ae@gmail.com>
2018-12-02 14:04:26 -0600

    complex number multiplication

--- src/leetcode/ComplexNumberMul_537.java
+++ src/leetcode/ComplexNumberMul_537.java
@@ -36,4 +36,15 @@ public class ComplexNumberMul_537 {
         return sb.toString();
 
     }
+
+    public String advanceMul(String a, String b){
+        String x[] = a.split("\\+|i");
+        String y[] = b.split("\\+|i");
+        int a_real = Integer.parseInt(x[0]);
+        int a_img = Integer.parseInt(x[1]);
+        int b_real = Integer.parseInt(y[0]);
+        int b_img = Integer.parseInt(y[1]);
+        return (a_real * b_real - a_img * b_img) + "+" + (a_real * b_img + a_img * b_real) + "i";
+
+    }
 }
