0eb417500b7af13db4233e524a156566773f7889
Omar Faroque <faroque.ae@gmail.com>
2018-12-01 19:55:50 -0600

    masking personal information submission

--- src/leetcode/MaskingPI.java
+++ src/leetcode/MaskingPI.java
@@ -5,31 +5,63 @@ public class MaskingPI {
         MaskingPI mpi = new MaskingPI();
         String in1 = "LeetCode@LeetCode.com";
         String in2="AB@qq.com";
+        String in3="1(234)567-890";
+        String in4="86-(10)12345678";
+        String in5="+5(4266)719-677-";
+        String in6="JackAndJill@Gmail.Com";
+        /*
         System.out.println(mpi.maskPII(in1));
         System.out.println(mpi.maskPII(in2));
+        System.out.println(mpi.maskPII(in3));
+        System.out.println(mpi.maskPII(in4));
+        System.out.println(mpi.maskPII(in5));
+        */
+        System.out.println(mpi.maskPII(in6));
 
     }
 
     public String maskPII(String S) {
-        if(Character.isDigit(S.charAt(S.length()-1)))
-            return processNumber(S);
+        String s= S.toLowerCase();
+        String replaced= s.replaceAll("[^0-9a-zA-Z@.]","");
+        if(Character.isDigit(replaced.charAt(replaced.length()-1)))
+            return processNumber(replaced);
         else
-            return processEmail(S);
-
+            return processEmail(replaced);
     }
 
-    private String processNumber(String s) {
-        return null;
+    private String processNumber(String fInput) {
+        //String fInput= s.replaceAll("[^0-9]","");
+        String extra="";
+        StringBuilder sb=new StringBuilder();
+        if(fInput.length()<10)
+            return extra;
+        if(fInput.length()>10){
+            if(fInput.length()==12){
+                extra="+**-";
+            }else if(fInput.length()==13){
+                extra="+***-";
+            }else {
+                extra="+*-";
+            }
+
+        }
+        sb.append(extra);
+        sb.append("***-***-");
+        sb.append(fInput.substring(fInput.length()-4));
+
+
+        //System.out.println(fInput);
+        return sb.toString();
     }
 
-    private String processEmail(String S) {
-        String str = S.toLowerCase();
+    private String processEmail(String str) {
+        //String str = S.toLowerCase();
         String[] split= str.split("@");
         StringBuilder sb=new StringBuilder();
 
         sb.append(split[0].charAt(0));
         sb.append("*****");
-        sb.append(split[0].charAt(split.length-1));
+        sb.append(split[0].charAt(split[0].length()-1));
         sb.append("@");
         sb.append(split[1]);
         return sb.toString();
