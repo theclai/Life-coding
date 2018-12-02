b99407f230075466bd61b333c2ab48cf9f75aead
Omar Faroque <faroque.ae@gmail.com>
2018-12-02 13:42:47 -0600

    adding better algorithm

--- src/leetcode/LongestUncommonSubsequence_522.java
+++ src/leetcode/LongestUncommonSubsequence_522.java
@@ -35,4 +35,33 @@ public class LongestUncommonSubsequence_522 {
         }
         return res;
     }
+
+    /**
+     *By some analysis, we can note that if longest uncommon subsequence is there, then it will always be one of the string
+     * from the given list of strings. Using this idea, we can check each string that
+     * whether it is a subsequence of any other string. If a string is not a subsequence of any other string i.e. it is uncommon ,
+     *  we will return maximum length string out of them. If no string found, we will return .
+     */
+    public boolean isSubsequence(String x, String y) {
+        int j = 0;
+        for (int i = 0; i < y.length() && j < x.length(); i++)
+            if (x.charAt(j) == y.charAt(i))
+                j++;
+        return j == x.length();
+    }
+
+    public int findLUSlength1(String[] strs) {
+        int res = -1;
+        for (int i = 0, j; i < strs.length; i++) {
+            for (j = 0; j < strs.length; j++) {
+                if (j == i)
+                    continue;
+                if (isSubsequence(strs[i], strs[j]))
+                    break;
+            }
+            if (j == strs.length)
+                res = Math.max(res, strs[i].length());
+        }
+        return res;
+    }
 }
