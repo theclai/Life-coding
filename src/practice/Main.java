20a699837395c53e9885ae8fce814721c2e90a25
nanofaroque <faroque.ae@gmail.com>
2018-12-03 10:20:27 -0600

    longest palindromic subsequence in dp

--- src/practice/Main.java
+++ src/practice/Main.java
@@ -1,8 +1,16 @@
 package practice;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Stack;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        Stack s=new Stack();
+
+        Deque a=new ArrayDeque();
     }
 }
