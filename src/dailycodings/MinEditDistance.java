0e51653842d1d2396daf9672312b9b36a186347c
nanofaroque <faroque.ae@gmail.com>
2018-12-04 14:05:15 -0600

    min distance

---
+++ src/dailycodings/MinEditDistance.java
@@ -0,0 +1,31 @@
+package dailycodings;
+
+public class MinEditDistance {
+    public static void main(String[] args) {
+        MinEditDistance med = new MinEditDistance();
+        System.out.println(med.minDistance("god","dog"));
+    }
+
+    public int minDistance(String a, String b) {
+        int r = a.length();
+        int c = b.length();
+        int[][] dp = new int[r][c];
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if(i==0)
+                    dp[i][j]=j+1;
+                else if(j==0)
+                    dp[i][j]=dp[i-1][j]+1;
+                else if(a.charAt(i)==b.charAt(j))
+                    dp[i][j]=dp[i-1][j-1];
+                else
+                    dp[i][j]=1+Math.min(Math.min(dp[i][j-1],  // Insert
+                            dp[i-1][j]),  // Remove
+                            dp[i-1][j-1]);
+
+            }
+        }
+        return dp[r - 1][c - 1];
+
+    }
+}
