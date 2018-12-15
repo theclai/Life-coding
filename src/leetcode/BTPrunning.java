1909a5d46f3b24aa6aa7b11002be6787775caacb
Omar Faroque (Anik) <faroque.ae@gmail.com>
2018-12-14 19:41:06 -0600

    bt prunning

---
+++ src/leetcode/BTPrunning.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+public class BTPrunning {
+    public TreeNode pruneTree(TreeNode root) {
+        return containsOne(root) ? root : null;
+    }
+
+    public boolean containsOne(TreeNode node) {
+        if (node == null) return false;
+
+        boolean a1 = containsOne(node.left);
+        boolean a2 = containsOne(node.right);
+
+        if (!a1) node.left = null;
+        if (!a2) node.right = null;
+
+        return node.val == 1 || a1 || a2;
+    }
+}
