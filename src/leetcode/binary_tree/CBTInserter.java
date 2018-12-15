974b5d370c652dfc96e9d6a4c9b46497958877ed
Omar Faroque (Anik) <faroque.ae@gmail.com>
2018-12-15 14:14:00 -0600

    cbtinserter

---
+++ src/leetcode/binary_tree/CBTInserter.java
@@ -0,0 +1,47 @@
+package leetcode.binary_tree;
+
+import leetcode.TreeNode;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class CBTInserter {
+    TreeNode root;
+    Deque<TreeNode> deque;
+
+    public CBTInserter(TreeNode root) {
+        this.root = root;
+        deque = new LinkedList();
+        Queue<TreeNode> queue = new LinkedList();
+        queue.offer(root);
+
+        // BFS to populate deque
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node.left == null || node.right == null)
+                deque.offerLast(node);
+            if (node.left != null)
+                queue.offer(node.left);
+            if (node.right != null)
+                queue.offer(node.right);
+        }
+    }
+
+    public int insert(int v) {
+        TreeNode node = deque.peekFirst();
+        deque.offerLast(new TreeNode(v));
+        if (node.left == null)
+            node.left = deque.peekLast();
+        else {
+            node.right = deque.peekLast();
+            deque.pollFirst();
+        }
+
+        return node.val;
+    }
+
+    public TreeNode get_root() {
+        return root;
+    }
+}
