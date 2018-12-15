ed1a3277f7e864c55c33ecc853c4648334c88e89
Omar Faroque (Anik) <faroque.ae@gmail.com>
2018-12-14 18:58:23 -0600

    search

---
+++ src/leetcode/BSTSearch.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+public class BSTSearch {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root==null)
+            return root;
+        if(root.val==val)
+            return root;
+        else if(val>root.val)
+            return searchBST(root.right,val);
+        else
+            return searchBST(root.left,val);
+
+    }
+
+}
