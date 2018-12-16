20dc86f5a8e5de6264e33339ec967194c79d60b3
Omar Faroque (Anik) <faroque.ae@gmail.com>
2018-12-15 21:15:03 -0600

    duplicates

---
+++ src/leetcode/binary_tree/FindDubplicateSubtree.java
@@ -0,0 +1,28 @@
+package leetcode.binary_tree;
+
+import leetcode.TreeNode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FindDubplicateSubtree {
+    Map<String, Integer> count;
+    List<TreeNode> ans;
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        count = new HashMap();
+        ans = new ArrayList();
+        collect(root);
+        return ans;
+    }
+
+    public String collect(TreeNode node) {
+        if (node == null) return "#";
+        String serial = node.val + "," + collect(node.left) + "," + collect(node.right);
+        count.put(serial, count.getOrDefault(serial, 0) + 1);
+        if (count.get(serial) == 2)
+            ans.add(node);
+        return serial;
+    }
+}
