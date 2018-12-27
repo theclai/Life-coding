3a37df2ecb5f5592c708b2445f1aec8a7e79aa43
Omar Faroque (Anik) <faroque.ae@gmail.com>
2018-12-27 16:56:00 -0600

    pascal triangle

---
+++ src/coderbyte/PascalTriangle.java
@@ -0,0 +1,25 @@
+package coderbyte;
+
+public class PascalTriangle {
+    public static void printPascal(int n)
+    {
+        for(int line = 1; line <= n; line++)
+        {
+
+            int C=1;// used to represent C(line, i)
+            for(int i = 1; i <= line; i++)
+            {
+                // The first value in a line is always 1
+                System.out.print(C+" ");
+                C = C * (line - i) / i;
+            }
+            System.out.println();
+        }
+    }
+
+    //Diver code
+    public static void main (String[] args) {
+        int n = 5;
+        printPascal(n);
+    }
+}
