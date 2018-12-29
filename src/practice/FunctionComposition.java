79a48fe4041e2d58b57a368e6f3b7e486c5f54cb
Omar Faroque (Anik) <faroque.ae@gmail.com>
2018-12-29 12:16:31 -0600

    function composition

---
+++ src/practice/FunctionComposition.java
@@ -0,0 +1,26 @@
+package practice;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.function.Function;
+
+public class FunctionComposition {
+    Function<String, String> demoFunc;
+
+    @BeforeEach
+    public void init() {
+        demoFunc = str -> {
+            String extra = "Hello ";
+            return extra + str;
+        };
+    }
+
+    @Test
+    public void test() {
+        String[] in = new String[]{"Omar", "Asma", "Eshika"};
+        Arrays.stream(in).map(demoFunc).forEach(output -> System.out.println(output));
+
+    }
+}
