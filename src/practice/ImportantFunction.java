7f5f331a8a37328f0957ce7335bb9dcacfd3e2a0
Omar Faroque (Anik) <faroque.ae@gmail.com>
2019-01-02 17:37:42 -0600

    bifunction

--- src/practice/ImportantFunction.java
+++ src/practice/ImportantFunction.java
@@ -2,10 +2,17 @@ package practice;
 
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ImportantFunction {
+    static BiFunction<Integer,Integer,Integer> biFunction=(a,b)->a+b;
+    static BiFunction<Integer,Integer,Boolean> compare=(a,b)->a>b?true:false;
+    static BiFunction<String,String,String> concate=(x,y)->{
+        String hello="Hello ";
+        return hello+ x.concat(y);
+    };
     public static void main(String[] args) {
         String s="i love shbanglade";
         String out=Stream.of(s.split("")).sorted().collect(Collectors.joining());
@@ -23,5 +30,14 @@ public class ImportantFunction {
         map.entrySet().stream().forEach(entry->{
             System.out.println("Key: "+entry.getKey()+"value: "+entry.getValue());
         });
+
+        int result=biFunction.apply(100,100);
+        System.out.println(result);
+        System.out.println(biFunction.apply(10,10));
+
+        System.out.println(compare.apply(10,12));
+        System.out.println(compare.apply(12,12));
+
+        System.out.println(concate.apply("Asma","Faroque"));
     }
 }
