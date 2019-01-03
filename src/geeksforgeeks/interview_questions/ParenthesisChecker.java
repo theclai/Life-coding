7683cbe07b886510673593f0c031527c501b4f0b
Omar Faroque (Anik) <faroque.ae@gmail.com>
2019-01-02 23:24:51 -0600

    parenthesis checker

---
+++ src/geeksforgeeks/interview_questions/ParenthesisChecker.java
@@ -0,0 +1,45 @@
+package geeksforgeeks.interview_questions;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class ParenthesisChecker {
+    public static void main(String[] args) {
+        String in = "[()]{}{[()()]()}";
+        System.out.println(isValid(in));
+
+        //System.out.println(isValid("{{}}"));
+    }
+
+    public static boolean isValid(String in) {
+        Deque<Character> stack = new ArrayDeque<>();
+        for (int i = 0; i < in.length(); i++) {
+            if (!stack.isEmpty()) {
+                Character c = stack.peekFirst();
+                if (helper(in.charAt(i)) == c)
+                    stack.removeFirst();
+                else
+                    stack.addFirst(c);
+            } else {
+                stack.addFirst(in.charAt(i));
+            }
+        }
+        return stack.size() == 0;
+    }
+
+    public static Character helper(Character character) {
+        Character op = null;
+        switch (character) {
+            case ']':
+                op = '[';
+                break;
+            case '}':
+                op = '{';
+                break;
+            case ')':
+                op = '(';
+                break;
+        }
+        return op;
+    }
+}
