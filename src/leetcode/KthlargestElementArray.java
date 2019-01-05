0f87bcadec0cbd6a907930a3c2f4d7ec2e4c7ebf
Omar Faroque (Anik) <faroque.ae@gmail.com>
2019-01-05 15:48:49 -0600

    k th largest element

---
+++ src/leetcode/KthlargestElementArray.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class KthlargestElementArray {
+    public static void main(String[] args) {
+        int[] in=new int[]{3,2,1,5,6,4};
+        System.out.println(findKthLargest(in,2));
+    }
+    public static int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(k);
+
+        Arrays.stream(nums).forEach(value -> {
+            pq.add(value);
+            if(pq.size()>k)
+                pq.poll();
+        });
+        return pq.peek();
+    }
+}
