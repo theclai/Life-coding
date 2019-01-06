dc2f2753d5892da7d1ded4233e0713701205e6a1
Omar Faroque (Anik) <faroque.ae@gmail.com>
2019-01-05 20:47:21 -0600

    powerful integer

---
+++ src/leetcode/contests/PowerFulIntegers.java
@@ -0,0 +1,45 @@
+package leetcode.contests;
+
+import javafx.collections.transformation.SortedList;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class PowerFulIntegers {
+    public static void main(String[] args) {
+        ArrayList<Integer> op= (ArrayList<Integer>) powerfulIntegers(2,3,10);
+        op.forEach(val-> System.out.println(val));
+    }
+    public static List<Integer> powerfulIntegers(int x, int y, int bound) {
+        if(bound==0) return null;
+        int xLimit=0;
+        while(Math.pow(x,xLimit)<=bound-1){
+            xLimit++;
+        }
+
+        System.out.println(xLimit);
+
+        int yLimit=0;
+        while(Math.pow(y,yLimit)<=bound-1){
+            yLimit++;
+        }
+
+
+        System.out.println(yLimit);
+
+        Set list=new HashSet();
+        for(int i=0;i<xLimit;i++){
+            for (int j = 0; j < yLimit; j++) {
+                double temp=Math.pow(x,i)+Math.pow(y,j);
+                if(temp<=10){
+                    list.add((int) temp);
+                }
+            }
+        }
+
+        return new ArrayList<>(list);
+
+    }
+}
