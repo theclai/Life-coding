e9435ba4cefc6cc824bf507cc43257cb5747260b
Omar Faroque (Anik) <faroque.ae@gmail.com>
2019-01-06 21:11:55 -0600

    length of longest substring

---
+++ src/program_creek/LengthOfLongestSubstring.java
@@ -0,0 +1,34 @@
+package program_creek;
+
+import java.util.HashSet;
+
+public class LengthOfLongestSubstring {
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int result = 0;
+        int k = 0;
+        HashSet<Character> set = new HashSet<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!set.contains(c)) {
+                set.add(c);
+                result = Math.max(result, set.size());
+            } else {
+                while (k < i) {
+                    if (s.charAt(k) == c) {
+                        k++;
+                        break;
+                    } else {
+                        set.remove(s.charAt(k));
+                        k++;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+}
