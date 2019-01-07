c8ef7f460c7eff26656feebb94b7aab5d507f549
Omar Faroque (Anik) <faroque.ae@gmail.com>
2019-01-07 13:20:13 -0600

    longest increasing sequence

--- src/program_creek/LIS.java
+++ src/program_creek/LIS.java
@@ -1,4 +1,27 @@
 package program_creek;
 
+import java.util.Arrays;
+
 public class LIS {
+    public static void main(String[] args) {
+        int arr[] = { 10, 22, 9, 33, 21, 50, 41, 60 };
+        int n = arr.length;
+        System.out.println("Length of lis is " + lis( arr, n ) + "\n" );
+    }
+
+    private static int lis(int[] arr, int n) {
+        int[] lis=new int[n];
+        Arrays.fill(lis,1);
+        int max=Integer.MIN_VALUE;
+
+        for (int i = 1; i <n ; i++) {
+            for (int j = i-1; j >=0 ; j--) {
+                if(arr[j]<=arr[i]){
+                    lis[i]=Math.max(lis[j]+1,lis[i]);
+                    max=Math.max(max,lis[i]);
+                }
+            }
+        }
+        return max;
+    }
 }
