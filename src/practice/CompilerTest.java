665d532c706bdbc8185ce3a532b026e65c5001a7
Omar Faroque (Anik) <faroque.ae@gmail.com>
2019-01-10 20:52:46 -0600

    random test

---
+++ src/practice/CompilerTest.java
@@ -0,0 +1,72 @@
+package practice;
+
+public class CompilerTest {
+    public static void main(String[] args) {
+        int[][] a = new int[][]{
+                {1, 2, 3, 4, 5, 6, 7, 8, 9, 100},
+                {1, 2, 3, 4, 5, 6, 7, 8, 9, 100},
+                {1, 2, 3, 4, 5, 6, 7, 8, 9, 100},
+                {1, 2, 3, 4, 5, 6, 7, 8, 9, 100},
+                {1, 2, 3, 4, 5, 6, 7, 8, 9, 100},
+                {1, 2, 3, 4, 5, 6, 7, 8, 9, 100},
+                {1, 2, 3, 4, 5, 6, 7, 8, 9, 100},
+                {1, 2, 3, 4, 5, 6, 7, 8, 9, 100},
+                {1, 2, 3, 4, 5, 6, 7, 8, 9, 100},
+                {1, 2, 3, 4, 5, 6, 7, 8, 9, 100}
+        };
+
+        int[][] b = new int[][]{
+                {11, 22, 33, 44, 55, 66, 77, 88, 99, 100},
+                {11, 22, 33, 44, 55, 66, 77, 88, 99, 100},
+                {11, 22, 33, 44, 55, 66, 77, 88, 99, 100},
+                {11, 22, 33, 44, 55, 66, 77, 88, 99, 100},
+                {11, 22, 33, 44, 55, 66, 77, 88, 99, 100},
+                {11, 22, 33, 44, 55, 66, 77, 88, 99, 100},
+                {11, 22, 33, 44, 55, 66, 77, 88, 99, 100},
+                {11, 22, 33, 44, 55, 66, 77, 88, 99, 100},
+                {11, 22, 33, 44, 55, 66, 77, 88, 99, 100},
+                {11, 22, 33, 44, 55, 66, 77, 88, 99, 100}
+
+        };
+
+
+        long start = (int) (System.currentTimeMillis() * 1000);
+        computeByRow(a, b);
+        long end = (int) (System.currentTimeMillis() * 1000);
+        long diff=end-start;
+        System.out.println(diff);
+
+
+        long start1 = (int) (System.currentTimeMillis() * 1000);
+        computeByCol(a, b);
+        long end1 = (int) (System.currentTimeMillis() * 1000);
+        long diff1=end1-start1;
+        System.out.println(diff1);
+
+        if(diff1-diff>0)
+            System.out.println("takes more time");
+        else
+            System.out.println("less");
+
+    }
+
+    private static void computeByCol(int[][] a, int[][] b) {
+        int[][] c = new int[10][10];
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                c[j][i] = a[j][i] + b[j][i];
+            }
+
+        }
+    }
+
+    private static void computeByRow(int[][] a, int[][] b) {
+        int[][] c = new int[10][10];
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                c[i][j] = a[i][j] + b[i][j];
+            }
+
+        }
+    }
+}
