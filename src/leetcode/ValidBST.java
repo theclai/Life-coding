db69049cba994974d54abf856777f873832716a0
Omar Faroque (Anik) <faroque.ae@gmail.com>
2019-01-12 19:15:35 -0600

    validate bst

---
+++ src/leetcode/ValidBST.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+public class ValidBST {
+    public boolean isBSTHelper(TreeNode node, Integer lower_limit, Integer upper_limit) {
+        if ((lower_limit != null) && (node.val <= lower_limit))
+            return false;
+        if ((upper_limit != null) && (upper_limit <= node.val))
+            return false;
+
+        boolean left = node.left != null ? isBSTHelper(node.left, lower_limit, node.val) : true;
+        if (left) {
+            boolean right = node.right != null ? isBSTHelper(node.right, node.val, upper_limit) : true;
+            return right;
+        } else
+            return false;
+    }
+
+    public boolean isValidBST(TreeNode root) {
+        if (root == null)
+            return true;
+
+        return isBSTHelper(root, null, null);
+    }
+}
