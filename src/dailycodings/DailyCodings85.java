a0ead6902b19af943a073ef2797cc12219aa7775
Omar Faroque (Anik) <faroque.ae@gmail.com>
2019-01-27 19:40:37 -0600

    wrong implementation

---
+++ src/dailycodings/DailyCodings85.java
@@ -0,0 +1,16 @@
+package dailycodings;
+
+public class DailyCodings85 {
+    public static void main(String[] args) {
+        int x=34;
+        int y=67;
+        int b=0;
+
+        System.out.println(test(x,y,0));
+    }
+    public static int test(int x,int y,int b){
+        if((y^b)==b) return y;
+        else
+            return x;
+    }
+}
