44134a0d2b1fbe4795ed720d15c842c4e8ef3550
Omar Faroque (Anik) <faroque.ae@gmail.com>
2019-01-27 19:24:57 -0600

    adding unit test

--- src/dailycodings/NumberOfIsland.java
+++ src/dailycodings/NumberOfIsland.java
@@ -1,10 +1,16 @@
 package dailycodings;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class NumberOfIsland {
+    NumberOfIsland island;
     static int[][] in;
     static int[][] path;
 
-    public static void main(String[] args) {
+    @BeforeEach
+    public void init() {
         in = new int[][]{
                 {1, 0, 0, 0, 0},
                 {0, 0, 1, 1, 0},
@@ -14,16 +20,16 @@ public class NumberOfIsland {
                 {1, 1, 0, 0, 1}
         };
 
-        path = new int[][]{
-                {-1, 0},
-                {1, 0},
-                {0, -1},
-                {0, 1}
-        };
-        System.out.println(numberOfIsland(in));
+        path = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        island = new NumberOfIsland();
     }
 
-    private static int numberOfIsland(int[][] in) {
+    @Test
+    public void countIslandTesst() {
+        Assertions.assertEquals(4, island.numberOfIsland(in));
+    }
+
+    private int numberOfIsland(int[][] in) {
         int total = 0;
         for (int i = 0; i < in.length; i++) {
             for (int j = 0; j < in[0].length; j++) {
@@ -36,7 +42,7 @@ public class NumberOfIsland {
         return total;
     }
 
-    private static void helper(int[][] in, int r, int c) {
+    private void helper(int[][] in, int r, int c) {
         if (r < 0 || r >= 6 || c < 0 || c >= 5 || in[r][c] == 0 || in[r][c] == 2)
             return;
         in[r][c] = 2;
