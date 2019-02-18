489bdb53da2520f999960e6ef563e00e35788451
Omar Faroque (Anik) <faroque.ae@gmail.com>
2019-02-18 11:36:07 -0600

    stack implementation

--- src/dailycodings/PalindromeSinglyLinkedList.java
+++ src/dailycodings/PalindromeSinglyLinkedList.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Iterator;
 import java.util.LinkedList;
 
@@ -19,14 +21,14 @@ public class PalindromeSinglyLinkedList {
         head.next = new ListNode(4);
         head.next.next = new ListNode(3);
         head.next.next.next = new ListNode(4);
-        head.next.next.next.next = new ListNode(1);
-
-
+        head.next.next.next.next = new ListNode(2);
     }
 
     @Test
     public void psll_positive_test() {
         Assertions.assertEquals(true,isPalindrom(head));
+        Assertions.assertEquals(true,isPalindromByStack(head));
+
     }
 
     public boolean isPalindrom(ListNode head) {
@@ -37,13 +39,25 @@ public class PalindromeSinglyLinkedList {
             res.append(seperator);
             head = head.next;
         }
-
         res.deleteCharAt(res.length()-1);
-
-
         String str=res.toString();
         String str1=res.reverse().toString();
         return str.equalsIgnoreCase(str1);
+    }
 
+
+    public boolean isPalindromByStack(ListNode head) {
+        ListNode tempHead=head;
+        Deque stack=new ArrayDeque();
+        while (head != null) {
+            stack.push(head.val);
+            head = head.next;
+        }
+        while (!stack.isEmpty() && tempHead!=null){
+            if(tempHead.val!=(int)stack.poll())
+                return false;
+            tempHead=tempHead.next;
+        }
+        return true;
     }
 }
