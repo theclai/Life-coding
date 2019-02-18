2c66811c595ff060776b402917e61145f8db8b62
Omar Faroque (Anik) <faroque.ae@gmail.com>
2019-02-18 10:51:32 -0600

    validation of palindrom in a signly linked list

---
+++ src/dailycodings/PalindromeSinglyLinkedList.java
@@ -0,0 +1,49 @@
+package dailycodings;
+
+import leetcode.ListNode;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class PalindromeSinglyLinkedList {
+    PalindromeSinglyLinkedList palindromeSinglyLinkedList;
+    ListNode head;
+
+    @BeforeEach
+    public void init() {
+        palindromeSinglyLinkedList = new PalindromeSinglyLinkedList();
+        head = new ListNode(1);
+        head.next = new ListNode(4);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(1);
+
+
+    }
+
+    @Test
+    public void psll_positive_test() {
+        Assertions.assertEquals(true,isPalindrom(head));
+    }
+
+    public boolean isPalindrom(ListNode head) {
+        String seperator = "-";
+        StringBuilder res = new StringBuilder();
+        while (head != null) {
+            res.append(head.val);
+            res.append(seperator);
+            head = head.next;
+        }
+
+        res.deleteCharAt(res.length()-1);
+
+
+        String str=res.toString();
+        String str1=res.reverse().toString();
+        return str.equalsIgnoreCase(str1);
+
+    }
+}
