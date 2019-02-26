6d76547496cefc5eb296cce846cf93d21f647323
Omar Faroque (Anik) <faroque.ae@gmail.com>
2019-02-25 22:59:36 -0600

    bit masking

---
+++ src/dailycodings/UnsignedInteger.java
@@ -0,0 +1,14 @@
+package dailycodings;
+
+import sun.security.util.BitArray;
+
+public class UnsignedInteger {
+    public static void main(String[] args) {
+
+        int even = 0b10101010;
+        int odd = 0b01010101;
+
+        int x=226;
+        System.out.println((x&even)>>1 | (x & odd));
+    }
+}
