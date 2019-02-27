916842b9b6d6c49c61f81afbd593a5870fd85f4a
Omar Faroque (Anik) <faroque.ae@gmail.com>
2019-02-26 22:28:30 -0600

    anagram substring search

---
+++ src/dailycodings/AnagramSubstringSearch.java
@@ -0,0 +1,47 @@
+package dailycodings;
+
+public class AnagramSubstringSearch {
+    static final int MAX = 256;
+
+    public static void main(String[] args) {
+        String txt = "BACDGABCDA";
+        String pat = "ABCD";
+        search(pat, txt);
+
+    }
+
+    static boolean compare(char arr1[], char arr2[]) {
+        for (int i = 0; i < MAX; i++)
+            if (arr1[i] != arr2[i])
+                return false;
+        return true;
+    }
+
+    public static void search(String pat, String txt) {
+        int M = pat.length();
+        int N = txt.length();
+
+        char[] countP = new char[MAX];
+        char[] countTW = new char[MAX];
+
+        for (int i = 0; i < M; i++) {
+            (countP[pat.charAt(i)])++;
+            (countTW[txt.charAt(i)])++;
+        }
+
+        for (int i = M; i < N; i++) {
+            if (compare(countP, countTW))
+                System.out.println("Found at Index " +
+                        (i - M));
+            (countTW[txt.charAt(i)])++;
+
+            // Remove the first character of previous
+            // window
+            countTW[txt.charAt(i - M)]--;
+        }
+
+        if (compare(countP, countTW))
+            System.out.println("Found at Index " +
+                    (N - M));
+    }
+}
