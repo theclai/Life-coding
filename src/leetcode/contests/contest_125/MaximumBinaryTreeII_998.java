f2b7eef57430d3c12eb60eb71cc30214523a829d
Omar Faroque (Anik) <faroque.ae@gmail.com>
2019-02-27 22:26:47 -0600

    adding extra comment

--- src/leetcode/contests/contest_125/MaximumBinaryTreeII_998.java
+++ src/leetcode/contests/contest_125/MaximumBinaryTreeII_998.java
@@ -4,21 +4,43 @@ import leetcode.TreeNode;
 
 public class MaximumBinaryTreeII_998 {
     public TreeNode insertIntoMaxTree(TreeNode root, int val) {
+        /**
+         * Create a dummy node
+         * */
         TreeNode dummy = new TreeNode(Integer.MAX_VALUE);
+        /**
+         * Set current node to its right node
+         * */
         dummy.right = root;
+        /**
+         * Insert new node. We need access of the parent node
+         * */
         ins(dummy, root, new TreeNode(val));
+        /**
+         * Return node without dummy
+         * */
         return dummy.right;
     }
 
-    private void ins(TreeNode prev, TreeNode curr, TreeNode val) {
-        if (val.val > curr.val) {
-            val.left = curr;
-            prev.right = val;
+    /**
+     * @param prev    Parent Node
+     * @param curr    Root of the current node
+     * @param newNode New Node to insert
+     */
+    private void ins(TreeNode prev, TreeNode curr, TreeNode newNode) {
+        /**
+         * If newnode value is greater than the current node
+         * insert current node as left child of new node
+         * newnode would be right child of the parent node
+         * */
+        if (newNode.val > curr.val) {
+            newNode.left = curr;
+            prev.right = newNode;
         } else {
             if (curr.right != null) {
-                ins(curr, curr.right, val);
+                ins(curr, curr.right, newNode);
             } else {
-                curr.right = val;
+                curr.right = newNode;
             }
         }
     }
