297393425942074a3fe863a09771cd9876307969
Omar Faroque (Anik) <faroque.ae@gmail.com>
2019-02-28 12:55:19 -0600

    implementing similarity comparator

--- src/contest/hashcode_2019/FirstProblem.java
+++ src/contest/hashcode_2019/FirstProblem.java
@@ -3,27 +3,30 @@ package contest.hashcode_2019;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class FirstProblem {
 
     static class Photo {
         String orientation;
         int t;
-        List<String> tags;
+        String tags;
 
-        public  Photo(String orientation, int t, String[] ts) {
-            tags = new ArrayList<>();
+        public Photo(String orientation, int t, String[] ts) {
+            StringBuilder sb = new StringBuilder();
             this.orientation = orientation;
             this.t = t;
             for (int i = 0; i < ts.length; i++) {
-                tags.add(ts[i]);
+                sb.append(ts[i]);
+                sb.append(" ");
             }
 
+            this.tags = sb.toString().trim();
         }
     }
+
+    static List<Photo> horizontals;
+    static List<Photo> verticals;
 
     public static void main(String[] args) throws Exception {
         FileInputStream fstream = new FileInputStream("/Users/omarfaroque/Life-coding/src/contest/hashcode_2019/a_example.txt");
@@ -31,6 +34,8 @@ public class FirstProblem {
         int numOfInput = Integer.parseInt(br.readLine());
         System.out.println(numOfInput);
 
+        horizontals = new ArrayList<>();
+        verticals = new ArrayList<>();
         int i = 0;
 
         List<Photo> photos = new ArrayList<>();
@@ -38,12 +43,69 @@ public class FirstProblem {
             String str = br.readLine();
             String[] splited = str.split(" ");
             System.out.println(str);
-            Photo p = new Photo(splited[0], Integer.parseInt(splited[1]), Arrays.copyOfRange(splited, 2, splited.length));
-            photos.add(p);
+            if (splited[0] .equalsIgnoreCase("H")) {
+                Photo p = new Photo(splited[0], Integer.parseInt(splited[1]), Arrays.copyOfRange(splited, 2, splited.length));
+                horizontals.add(p);
+            } else {
+                Photo p = new Photo(splited[0], Integer.parseInt(splited[1]), Arrays.copyOfRange(splited, 2, splited.length));
+                verticals.add(p);
+            }
             i++;
         }
 
+        Collections.sort(horizontals,new TagsSimilarityComparator());
+        Collections.sort(verticals,new TagsSimilarityComparator());
+        System.out.println(photos);
 //Close the input stream
         fstream.close();
+    }
+
+    static class TagsSimilarityComparator implements Comparator<Photo> {
+        public int compare(Photo p1, Photo p2) {
+            return (int) similarity(p1.tags, p2.tags);
+        }
+    }
+
+    public static double similarity(String s1, String s2) {
+        String longer = s1, shorter = s2;
+        if (s1.length() < s2.length()) { // longer should always have greater length
+            longer = s2;
+            shorter = s1;
+        }
+        int longerLength = longer.length();
+        if (longerLength == 0) {
+            return 1.0; /* both strings are zero length */
+        }
+    /* // If you have Apache Commons Text, you can use it to calculate the edit distance:
+    LevenshteinDistance levenshteinDistance = new LevenshteinDistance();
+    return (longerLength - levenshteinDistance.apply(longer, shorter)) / (double) longerLength; */
+        return (int) (longerLength - editDistance(longer, shorter)) / (double) longerLength;
+    }
+
+    public static int editDistance(String s1, String s2) {
+        s1 = s1.toLowerCase();
+        s2 = s2.toLowerCase();
+
+        int[] costs = new int[s2.length() + 1];
+        for (int i = 0; i <= s1.length(); i++) {
+            int lastValue = i;
+            for (int j = 0; j <= s2.length(); j++) {
+                if (i == 0)
+                    costs[j] = j;
+                else {
+                    if (j > 0) {
+                        int newValue = costs[j - 1];
+                        if (s1.charAt(i - 1) != s2.charAt(j - 1))
+                            newValue = Math.min(Math.min(newValue, lastValue),
+                                    costs[j]) + 1;
+                        costs[j - 1] = lastValue;
+                        lastValue = newValue;
+                    }
+                }
+            }
+            if (i > 0)
+                costs[s2.length()] = lastValue;
+        }
+        return costs[s2.length()];
     }
 }
