096014197c66dd2c2c2030baba5f464c8d559c4a
Omar Faroque (Anik) <faroque.ae@gmail.com>
2019-03-19 13:07:23 -0500

    wrong implementation of 875

--- src/dailycodings/MaxProfitByKTimes_130.java
+++ src/dailycodings/MaxProfitByKTimes_130.java
@@ -1,4 +1,48 @@
 package dailycodings;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class MaxProfitByKTimes_130 {
+    MaxProfitByKTimes_130 mMaxProfit;
+
+    @BeforeEach
+    public void init() {
+        mMaxProfit = new MaxProfitByKTimes_130();
+    }
+
+    @Test
+    public void first_maxprofit_test() {
+        int result = mMaxProfit.maxProfit(new int[]{10, 22, 5, 75, 65, 80}, 6, 2);
+        Assertions.assertEquals(result, 87);
+    }
+    @Test
+    public void second_maxprofit(){
+        int result=mMaxProfit.maxProfit(new int[]{12, 14, 17, 10, 14, 13, 12, 15},8,2);
+        Assertions.assertEquals(result, 12);
+    }
+
+    public int maxProfit(int[] price,
+                         int n,
+                         int k) {
+        int total = 0;
+        int min = 0;
+        int max = 1;
+        int temp=0;
+        while (min < max && max < n && k > 0) {
+            if (price[max] < price[min]) {
+                total += price[max - 1] - price[min];
+                min = max;
+                max++;
+                k--;
+                temp=0;
+            } else {
+                max++;
+                if(max==n)
+                    total += price[max - 1] - price[min];
+            }
+        }
+        return total;
+    }
 }
--- src/leetcode/KokiEatingBananas_875.java
+++ src/leetcode/KokiEatingBananas_875.java
@@ -1,4 +1,67 @@
 package leetcode;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.IntStream;
+
 public class KokiEatingBananas_875 {
+    KokiEatingBananas_875 kokiEatingBananas_875;
+
+    @BeforeEach
+    public void init() {
+        kokiEatingBananas_875 = new KokiEatingBananas_875();
+    }
+
+    @Test
+    public void first_minEatingSpeed_positive() {
+        int res = kokiEatingBananas_875.minEatingSpeed(new int[]{3, 6, 7, 11}, 8);
+        Assertions.assertEquals(4, res);
+    }
+
+    @Test
+    public void second_minEatingSpeed_positive() {
+        int res = kokiEatingBananas_875.minEatingSpeed(new int[]{30, 11, 23, 4, 20}, 5);
+        Assertions.assertEquals(4, res);
+    }
+
+    public int minEatingSpeed(int[] piles, int H) {
+        /*piles = IntStream.of(piles)
+                .boxed()
+                .sorted(Comparator.reverseOrder())
+                .mapToInt(i -> i)
+                .toArray();*/
+        int sum = Arrays.stream(piles).sum();
+        int res = sum / H;
+        int rem = sum % H;
+        int speed = 0;
+        if (rem >= 0)
+            speed = res + 1;
+        int hour = Integer.MAX_VALUE;
+        while (hour != H) {
+            hour = Math.min(hour,computeSpeed(piles, speed));
+            speed++;
+        }
+        return speed-1;
+
+    }
+
+    private int computeSpeed(int[] piles, int speed) {
+        int i = 0;
+        int temp = 0;
+        int hour = 0;
+        while (i < piles.length) {
+            int item = piles[i];
+            if (item <= speed) {
+                i++;
+            } else {
+                piles[i] = speed - item;
+            }
+            hour++;
+        }
+        return hour+1;
+    }
 }
