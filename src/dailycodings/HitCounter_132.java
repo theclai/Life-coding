eae8e659dd9ddaabc150ac86f6463d1badf23711
Omar Faroque (Anik) <faroque.ae@gmail.com>
2019-04-02 22:23:32 -0500

    hit counter

---
+++ src/dailycodings/HitCounter_132.java
@@ -0,0 +1,54 @@
+package dailycodings;
+
+import java.util.LinkedHashMap;
+
+public class HitCounter_132 {
+    static class Counter {
+        long total;
+
+        public Counter(long total) {
+            this.total = total;
+        }
+
+        public long getTotal() {
+            return total;
+        }
+
+        public void setTotal(long total) {
+            this.total = total;
+        }
+    }
+
+    static LinkedHashMap<Long, Counter> counters;
+    static long total = 0;
+
+    public static void main(String[] args) {
+        counters = getCounterMap();
+        record(1);
+        record(1);
+        record(2);
+        record(3);
+        record(3);
+        System.out.println(total);
+        System.out.println(range(2,3));
+
+    }
+
+    private static LinkedHashMap<Long, Counter> getCounterMap() {
+        return new LinkedHashMap<>();
+    }
+
+    public static void record(long timestamp) {
+        total+=1;
+        counters.computeIfAbsent(timestamp, obj -> new Counter(total));
+        counters.get(timestamp).setTotal(total);
+    }
+
+    public static long total() {
+        return total;
+    }
+
+    public static long range(long low, long high) {
+        return counters.get(high).getTotal() - counters.get(low-1).getTotal();
+    }
+}
