570673415f272ef859c424575206e75a1fd7cc7b
Md Omar Faroque <faroque.ae@gmail.com>
2019-04-15 10:45:46 -0500

    max difference

--- src/leetcode/contests/contest_132/MaxdifferenceNodeAndAncestor.java
+++ src/leetcode/contests/contest_132/MaxdifferenceNodeAndAncestor.java
@@ -3,7 +3,8 @@ package leetcode.contests.contest_132;
 import leetcode.TreeNode;
 
 public class MaxdifferenceNodeAndAncestor {
-    static int maxVal;
+    static int maxdiff;
+
     public static void main(String[] args) {
        /* TreeNode root=new TreeNode(8);
         root.left=new TreeNode(3);
@@ -16,34 +17,27 @@ public class MaxdifferenceNodeAndAncestor {
         root.right.right=new TreeNode(14);
         root.right.right.left=new TreeNode(13);*/
 
-       TreeNode root=new TreeNode(2);
-       root.right=new TreeNode(0);
-       root.right.left=new TreeNode(1);
+        TreeNode root = new TreeNode(2);
+        root.right = new TreeNode(0);
+        root.right.left = new TreeNode(1);
         maxAncestorDiff(root);
-        System.out.println(maxVal);
+        System.out.println(maxdiff);
     }
 
     public static int maxAncestorDiff(TreeNode root) {
-       helper(root);
-       return maxVal;
+        if (null == root) return 0;
+        maxdiff = 0;
+        dfs(root, root.val, root.val);
+        return maxdiff;
     }
 
-    public static void helper(TreeNode root){
-        if(root==null)return;
-        int min= findMin(root,root.val);
-        int max=findMax(root,root.val);
-        maxVal= Math.max(maxVal,(Math.max(Math.abs(root.val-min),Math.abs(root.val-max))));
-
-        maxAncestorDiff(root.left);
-        maxAncestorDiff(root.right);
-    }
-    private static int findMin(TreeNode root,int min) {
-        if(root.left==null)return root.val;
-        return Math.min(min,findMin(root.left,min));
-    }
-
-    private static int findMax(TreeNode root,int max) {
-        if(root.right==null)return root.val;
-        return Math.max(max,findMin(root.right,max));
+    public static void dfs(TreeNode root, int min, int max) {
+        if (root == null) return;
+        int diff1 = Math.abs(root.val - min);
+        int diff2 = Math.abs(root.val - max);
+        maxdiff = Math.max(maxdiff, diff1);
+        maxdiff = Math.max(maxdiff, diff2);
+        dfs(root.left, Math.min(min, root.val), Math.max(max, root.val));
+        dfs(root.right, Math.min(min, root.val), Math.max(max, root.val));
     }
 }
