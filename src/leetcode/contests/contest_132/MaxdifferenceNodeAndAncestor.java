e2e52a0c1d8162b89973dcbbcd10b29f115749b7
Md Omar Faroque <faroque.ae@gmail.com>
2019-04-15 11:02:00 -0500

    add some comment

--- src/leetcode/contests/contest_132/MaxdifferenceNodeAndAncestor.java
+++ src/leetcode/contests/contest_132/MaxdifferenceNodeAndAncestor.java
@@ -6,20 +6,16 @@ public class MaxdifferenceNodeAndAncestor {
     static int maxdiff;
 
     public static void main(String[] args) {
-       /* TreeNode root=new TreeNode(8);
-        root.left=new TreeNode(3);
-        root.left.left=new TreeNode(1);
-        root.left.right=new TreeNode(6);
-        root.left.right.left=new TreeNode(4);
-        root.left.right.right=new TreeNode(7);
+        TreeNode root = new TreeNode(8);
+        root.left = new TreeNode(3);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(6);
+        root.left.right.left = new TreeNode(4);
+        root.left.right.right = new TreeNode(7);
 
-        root.right=new TreeNode(10);
-        root.right.right=new TreeNode(14);
-        root.right.right.left=new TreeNode(13);*/
-
-        TreeNode root = new TreeNode(2);
-        root.right = new TreeNode(0);
-        root.right.left = new TreeNode(1);
+        root.right = new TreeNode(10);
+        root.right.right = new TreeNode(14);
+        root.right.right.left = new TreeNode(13);
         maxAncestorDiff(root);
         System.out.println(maxdiff);
     }
@@ -33,10 +29,19 @@ public class MaxdifferenceNodeAndAncestor {
 
     public static void dfs(TreeNode root, int min, int max) {
         if (root == null) return;
+        /**
+         * Compute the difference with the root
+         * */
         int diff1 = Math.abs(root.val - min);
         int diff2 = Math.abs(root.val - max);
+        /**
+         * find the max difference from those value
+         * */
         maxdiff = Math.max(maxdiff, diff1);
         maxdiff = Math.max(maxdiff, diff2);
+        /**
+         * do dfs in both trees
+         * */
         dfs(root.left, Math.min(min, root.val), Math.max(max, root.val));
         dfs(root.right, Math.min(min, root.val), Math.max(max, root.val));
     }
